f32681c097587519cf368251671ecfe1842bea07
Douglas-g-s-Rocha <grocha0612@gmail.com>
2023-06-17 18:04:41 -0300

    correcao de alguns codigos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const contato = document.getElementById('contato');
 
 contato.addEventListener('click', function(event){ 
 
-    alert('Numero para ligação e WhatsApp: (11) 95511-4406') 
+    alert('Numero para ligação e WhatsApp: (11) 95511-4406 '  +  '                                    Email: grocha0612@gmail.com') 
 
 });
 
